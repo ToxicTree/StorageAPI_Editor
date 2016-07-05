45c4506b172e7af41e6f7dba052df376da6d8ccb
ToxicTree <zoi009900@gmail.com>
2016-07-05 20:03:13 +0200

    Editor & View: Refactoring

--- Editor.vue
+++ Editor.vue
@@ -2,87 +2,90 @@
 
     <form class="form-horizontal" id="editor">
 
-        <template v-if="$parent.table && $parent.row">
+        <template v-if="table && row">
 
             <div v-for="(index, value) in $parent.data[0]" class="form-group">
-                <label class="col-sm-offset-1 col-sm-2 control-label" for="{{index}}">{{index}}</label>
-                <div class="col-sm-7"><input class="form-control" name="{{index}}" v-model="value" :disabled="index=='id'"></div>
+                <label class="col-sm-offset-2 col-sm-2 control-label" for="{{index}}">
+                    {{index}}
+                </label>
+                <div class="col-sm-5">
+                    <input class="form-control" name="{{index}}" v-model="value" :disabled="index=='id'">
+                </div>
             </div>
             <div class="form-group" v-if="$parent.data[0].id">
-                <div class="col-sm-offset-3 col-sm-7" id="actions">
-                    <button type="button" class="btn" @click="click" data-path='/{{table}}/{{$parent.data[0].id}}'
-                            data-table='{{table}}' data-row={{$parent.data[0].id}}>Cancel</button>
-
-                    <button type="button" class="btn btn-primary" @click="click" data-path='/{{table}}/{{$parent.data[0].id}}' data-method='PUT'
-                            data-table='{{table}}' data-row={{$parent.data[0].id}}>Save</button>
+                <div class="col-sm-offset-4 col-sm-5">
+                    <div class="btn-group">
+                        <button @click="click" type="button" class="btn btn-default" data-path='/{{table}}/{{$parent.data[0].id}}'
+                                data-table='{{table}}' data-row={{$parent.data[0].id}}>Cancel</button>
+                        <button @click="click" type="button" class="btn btn-primary" data-path='/{{table}}/{{$parent.data[0].id}}'
+                                data-table='{{table}}' data-row={{$parent.data[0].id}} data-method='PUT'>Save</button>
+                    </div>
                 </div>
             </div>
 
         </template>
 
-        <template v-if="$parent.table && !$parent.row">
+        <template v-if="table && !row">
 
-            <table class="table">
-                <tr>
-                    <td>Table name:</td>
-                    <td>
-                        <input class="form-control" name="{{$parent.data[0].tablename}}" v-model="$parent.data[0].tablename">
-                        <input class="hidden" name="{{$parent.data[0].originalTablename}}" v-model="$parent.data[0].originalTablename"></div>
-                    </td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <th>Name</th>
-                    <th>Type</th>
-                    <th class="text-right">Actions</th>
-                </tr>
-                <tr v-for="(index, column) in $parent.data[0].columns">
-                    <template v-if="!column.fresh">
-                        <td>
-                            <input class="form-control" name="{{column.name}}" v-model="column.name" :disabled="column.originalName=='id'">
-                            <input class="hidden" name="{{column.originalName}}" v-model="column.originalName"></div>
-                        </td>
-                        <td>
-                            <select name="{{column.type}}" v-model="column.type" class="form-control" :disabled="column.originalName=='id'">
-                                <option v-for="type in types" value="{{type}}" :selected="type==column.type">{{type}}</option>
-                            </select>
-                            <input class="hidden" name="{{column.originalType}}" v-model="column.originalType"></div>
-                        </td>
-                        <td>
-                            <button v-if="column.originalName!='id'" type="button" class="btn pull-right" @click="remove(index)">Remove</button>
-                        </td>
-                    </template>
-                    <template v-else>
-                        <td>
-                            <input class="form-control" name="{{column.originalName}}" v-model="column.originalName">
-                        </td>
-                        <td>
-                            <select name="{{column.originalType}}" v-model="column.originalType" class="form-control">
-                                <option v-for="type in types" value="{{type}}" :selected="type==column.originalType">{{type}}</option>
-                            </select>
-                        </td>
-                        <td>
-                            <button type="button" class="btn pull-right" @click="remove(index)">Remove</button>
-                        </td>
-                    </template>
-                </tr>
-                <tr>
-                    <td colspan="3">
-                        <button type="button" class="btn pull-right" @click="add">Add</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="3">
-                        <div class="btn-group pull-right">
-                        <button type="button" class="btn" @click="click" data-path='/{{table}}'
+            <div class="form-group">
+                <label class="col-sm-offset-2 col-sm-2 control-label" for="{{index}}">
+                    Tablename
+                </label>
+                <div class="col-sm-5">
+                    <input class="form-control" name="{{$parent.data[0].tablename}}" v-model="$parent.data[0].tablename">
+                    <input class="hidden" name="{{$parent.data[0].originalTablename}}" v-model="$parent.data[0].originalTablename"></div>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-offset-2 col-sm-2 control-label" for="{{index}}">
+                    Columns
+                </label>
+                <div class="col-sm-6">
+                    <div v-for="(index, column) in $parent.data[0].columns" class="form-group">
+                        <template v-if="!column.fresh">
+                            <div class="col-sm-5">
+                                <input class="form-control" name="{{column.name}}" v-model="column.name" :disabled="column.originalName=='id'">
+                                <input class="hidden" name="{{column.originalName}}" v-model="column.originalName"></div>
+                            </div>
+                            <div class="col-sm-5">
+                                <select name="{{column.type}}" v-model="column.type" class="form-control" :disabled="column.originalName=='id'">
+                                    <option v-for="type in types" value="{{type}}" :selected="type==column.type">{{type}}</option>
+                                </select>
+                                <input class="hidden" name="{{column.originalType}}" v-model="column.originalType"></div>
+                            </div>
+                        </template>
+                        <template v-else>
+                            <div class="col-sm-5">
+                                <input class="form-control" name="{{column.originalName}}" v-model="column.originalName" :disabled="column.originalName=='id'">
+                                <input class="hidden" name="{{column.originalName}}" v-model="column.originalName"></div>
+                            </div>
+                            <div class="col-sm-5">
+                                <select name="{{column.originalType}}" v-model="column.originalType" class="form-control" :disabled="column.originalName=='id'">
+                                    <option v-for="type in types" value="{{type}}" :selected="type==column.originalType">{{type}}</option>
+                                </select>
+                            </div>
+                        </template>
+                        <div class="col-sm-2">
+                            <button type="button" class="btn btn-danger" @click="remove(index)">X</button>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-10 col-sm-2">
+                            <button type="button" class="btn btn-default" @click="add">+</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-4 col-sm-5">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-default" @click="click" data-path='/{{table}}'
                                 data-table='{{table}}' data-mode=''>Cancel</button>
-
                         <button type="button" class="btn btn-primary" @click="click" data-path='/{{table}}' data-method='PUT'
                                 data-table='{{table}}' data-mode=''>Save</button>
-                        </div>
-                    </td>
-                </tr>
-            </table>
+                    </div>
+                </div>
+            </div>
             
         </template>
 
@@ -93,9 +96,11 @@
 <script>
     export default {
 
-        props: [ 'table', 'row' ],
+        name: 'Editor',
+        
+        props: [ 'table', 'row', 'click' ],
 
-        data(){
+        data: function(){
             return {
                 types: [ 'INTEGER', 'TEXT' ]
             }
@@ -104,28 +109,18 @@
         methods: {
 
             remove(i){
+
                 this.$parent.data[0].columns.splice(i,1);
+
             },
 
             add(){
-                this.$parent.data[0].columns.push({fresh:true});
-            },
 
-            click(e){
-                this.$parent.navigate(
-                    e.target.getAttribute('data-path'),
-                    e.target.getAttribute('data-method'),
-                    e.target.getAttribute('data-mode'),
-                    e.target.getAttribute('data-table'),
-                    e.target.getAttribute('data-row')
-                );
+                this.$parent.data[0].columns.push({fresh:true});
+
             }
 
         }
 
     }
 </script>
-
-<style>
-
-</style>
--- View.vue
+++ View.vue
@@ -1,87 +1,103 @@
 <template>
 
-    <table class="table" v-if="!table && !row">
-        <tr><th>Table</th><th>Columns</th><th class="text-right">Tools</th></tr>
-        <tr v-for="object in $parent.data">
-            <td><a @click="click" href='{{$root.nav}}' data-path='/{{object.tablename}}' data-table='{{object.tablename}}'>{{object.tablename}}</a></td>
-            <td><span v-for="(index, column) in object.columns">{{ (index>0) ? ', '+column.name : column.name }}</span></td>
-            <td>
-                <button @click="click" type="button" class="btn pull-right" data-path='/{{object.tablename}}'
-                        data-table='{{object.tablename}}' data-mode='edit'>Edit</button>
-                <button @click="click" type="button" class="btn pull-right" data-path='/{{object.tablename}}'
-                        data-method='DELETE'>Kill</button>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="3">
-                <button @click="click" type="button" class="btn pull-right" data-path='/'
-                    data-method='post' data-mode='edit'>New</button>
-            </td>
-        </tr>
-    </table>
+    <form class="form-horizontal" id="view">
 
-    <table class="table" v-if="table && !row">
-        <tr><th v-for="column in $parent.data[0].columns">{{column.name}}</th><th class="text-right">Tools</th></tr>
-        <tr v-for="object in $parent.data[0].data">
-            <td v-for="(index, value) in object">
-                <a @click="click" v-if="index=='id'" href='{{$root.nav}}' data-path='/{{table}}/{{object.id}}'
-                   data-table='{{table}}' data-row='{{object.id}}'>#{{object.id}}</a>
-                <p v-else>{{value}}</p>
-            </td>
-            <td>
-                <button @click="click" type="button" class="btn pull-right" data-path='/{{table}}/{{object.id}}'
-                        data-table='{{table}}' data-row='{{object.id}}' data-mode='edit'>Edit</button>
-                <button @click="click" type="button" class="btn pull-right" data-path='/{{table}}/{{object.id}}'
-                        data-table='{{table}}' data-method='DELETE'>Kill</button>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="{{$parent.data[0].columns.length+1}}">
-                <button @click="click" type="button" class="btn pull-right" data-path='/{{table}}'
-                        data-table='{{table}}' data-mode='edit'>Edit</button>
-                <button @click="click" type="button" class="btn pull-right" data-path='/{{table}}'
-                        data-method='DELETE'>Kill</button>
-                <button @click="click" type="button" class="btn pull-right" data-path='/{{table}}'
-                        data-table='{{table}}' data-row='0' data-method='post' data-mode='edit'>New</button>
-            </td>
-        </tr>
-    </table>
+        <template v-if="$parent.table && $parent.row">
 
-    <table class="table" v-if="table && row">
-        <tr><th>Column</th><th>Value</th></tr>
-        <tr v-for="(index, value) in $parent.data[0]">
-            <td>{{index}}</td>
-            <td>{{value}}</td>
-        </tr>
-        <tr>
-            <td>Tools:</td>
-            <td>
-                <button @click="click" type="button" class="btn" data-path='/{{table}}/{{row}}'
-                        data-table='{{table}}' data-row='{{row}}' data-mode='edit'>Edit</button>
-                <button @click="click" type="button" class="btn" data-path='/{{table}}/{{row}}'
-                        data-table='{{table}}' data-method='DELETE'>Kill</button>
-            </td>
-        </tr>
-    </table>
+            <div v-for="(index, value) in $parent.data[0]" class="form-group">
+                <label class="col-sm-offset-2 col-sm-2 text-right" >
+                    {{index}}
+                </label>
+                <div class="col-sm-5">
+                    {{value}}
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-4 col-sm-5">
+                    <div class="btn-group">
+                        <button @click="click" type="button" class="btn btn-default" data-path='/{{table}}'
+                                data-table='{{table}}'>Back</button>
+                        <button @click="click" type="button" class="btn btn-default" data-path='/{{table}}/{{row}}'
+                                data-table='{{table}}' data-row='{{row}}' data-mode='edit'>Edit</button>
+                        <button @click="click" type="button" class="btn btn-danger" data-path='/{{table}}/{{row}}'
+                                data-table='{{table}}' data-method='DELETE'>Kill</button>
+                    </div>
+                </div>
+            </div>
+
+        </template>
+
+        <template v-if="table && !row">
+
+            <table class="table" >
+                <tr><th v-for="column in $parent.data[0].columns">{{column.name}}</th><th class="text-right">Tools</th></tr>
+                <tr v-for="object in $parent.data[0].data">
+                    <td v-for="(index, value) in object">
+                        <a @click="click" v-if="index=='id'" href='{{$root.nav}}' data-path='/{{table}}/{{object.id}}'
+                           data-table='{{table}}' data-row='{{object.id}}'>#{{object.id}}</a>
+                        <p v-else>{{value}}</p>
+                    </td>
+                    <td>
+                        <div class="btn-group pull-right">
+                            <button @click="click" type="button" class="btn btn-default" data-path='/{{table}}/{{object.id}}'
+                                    data-table='{{table}}' data-row='{{object.id}}' data-mode='edit'>Edit</button>
+                            <button @click="click" type="button" class="btn btn-danger" data-path='/{{table}}/{{object.id}}'
+                                    data-table='{{table}}' data-method='DELETE'>Kill</button>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="{{$parent.data[0].columns.length+1}}">
+                        <div class="btn-group pull-right">
+                            <button @click="click" type="button" class="btn btn-default" data-path='/{{table}}'
+                                    data-table='{{table}}' data-row='0' data-method='post' data-mode='edit'>New Post</button>
+                            <button @click="click" type="button" class="btn btn-default" data-path='/{{table}}'
+                                    data-table='{{table}}' data-mode='edit'>Edit Table</button>
+                            <button @click="click" type="button" class="btn btn-danger" data-path='/{{table}}'
+                                    data-method='DELETE'>Kill Table</button>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+
+        </template>
+
+        <template v-if="!table && !row">
+
+            <table class="table">
+                <tr><th>Table</th><th>Columns</th><th class="text-right">Tools</th></tr>
+                <tr v-for="object in $parent.data">
+                    <td><a @click="click" href='{{$root.nav}}' data-path='/{{object.tablename}}' data-table='{{object.tablename}}'>{{object.tablename}}</a></td>
+                    <td><span v-for="(index, column) in object.columns">{{ (index>0) ? ', '+column.name : column.name }}</span></td>
+                    <td>
+                        <div class="btn-group pull-right">
+                            <button @click="click" type="button" class="btn btn-default" data-path='/{{object.tablename}}'
+                                    data-table='{{object.tablename}}' data-mode='edit'>Edit</button>
+                            <button @click="click" type="button" class="btn btn-danger" data-path='/{{object.tablename}}'
+                                    data-method='DELETE'>Kill</button>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <button @click="click" type="button" class="btn btn-default pull-right" data-path='/'
+                            data-method='post' data-mode='edit'>New Table</button>
+                    </td>
+                </tr>
+            </table>
+
+        </template>
+
+    </form>
 
 </template>
 
 <script>
     export default {
 
-        props: [ 'table', 'row' ],
+        name: 'View',
 
-        methods: {
-            click(e){
-                this.$parent.navigate(
-                    e.target.getAttribute('data-path'),
-                    e.target.getAttribute('data-method'),
-                    e.target.getAttribute('data-mode'),
-                    e.target.getAttribute('data-table'),
-                    e.target.getAttribute('data-row')
-                );
-            }
-        }
+        props: [ 'table', 'row', 'click' ],
 
     }
 </script>
