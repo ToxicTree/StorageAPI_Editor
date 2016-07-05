b6c7848b55a38f7969b9f10c282ac54ab979403c
ToxicTree <zoi009900@gmail.com>
2016-07-05 19:59:23 +0200

    ApiNav: Refactored into v-if & props

--- ApiNav.vue
+++ ApiNav.vue
@@ -1,78 +1,65 @@
 <template>
 
-    <ol class="breadcrumb" id="apiPath">/</ol>
+    <ol class="breadcrumb" id="apiNav">
+        
+        <template v-if="!table && !row">
+
+            <li v-if="editMode">
+                <a href='{{$root.nav}}' @click.prevent="click" data-path='/'>Root</a>
+            </li>
+            <li v-else>
+                Root
+            </li>
+
+        </template>
+
+        <template v-if="table && !row">
+
+            <li>
+                <a href='{{$root.nav}}' @click.prevent="click" data-path='/'>Root</a>
+            </li>
+            <li v-if="editMode">
+                <a href='{{$root.nav}}' @click.prevent="click" data-path='/{{table}}' data-table='{{table}}'>{{table}}</a>
+            </li>
+            <li v-else>
+                {{table}}
+            </li>
+
+        </template>
+
+        <template v-if="table && row">
+
+            <li>
+                <a href='{{$root.nav}}' @click.prevent="click" data-path='/'>Root</a>
+            </li>
+            <li>
+                <a href='{{$root.nav}}' @click.prevent="click" data-path='/{{table}}' data-table='{{table}}'>{{table}}</a>
+            </li>
+            <li v-if="editMode">
+                <a href='{{$root.nav}}' @click.prevent="click" data-path='/{{table}}/{{row}}' data-table='{{table}}' data-row='{{row}}'>{{row}}</a>
+            </li>
+            <li v-else>
+                {{row}}
+            </li>
+
+        </template>
+
+        <template v-if="editMode">
+
+            <li>Edit</li>
+
+        </template>
+
+    </ol>
 
 </template>
 
 <script>
     export default {
 
-        events: {
-            'nav-update': function(apiPath,table,row) {
-                this.table = table;
-                this.row = row;
-                this.updateNav(apiPath);
-            }
-        },
-
-        data() {
-            return {
-                table: '',
-                row: null,
-                apiPath: '/'
-            }
-        },
-
-        methods: {
-            updateNav(apiPath) {
-
-                this.apiPath = apiPath;
-                var content = "";
-                var h = '', hierarchy = this.apiPath.split('/');
-
-                if (this.row) {
-
-                    content += "<li><a href='"+this.$root.nav+"' data-path='/'               data-table=''               >Root</a></li>";
-                    content += "<li><a href='"+this.$root.nav+"' data-path='/"+this.table+"' data-table='"+this.table+"' >"+this.table+"</a></li>";
-                    content += "<li class='active'>"+this.row+"</li>";
-                
-                } else if (this.table) {
-
-                    content += "<li><a href='"+this.$root.nav+"' data-path='/'               data-table=''               >Root</a></li>";
-                    content += "<li class='active'>"+this.table+"</li>";
-                
-                } else {
-
-                    content += "<li class='active'>Root</li>";
-                
-                }
-
-                content += "<li class='pull-right'><a data-path='"+this.apiPath+"' ";
-                
-                if (this.table)
-                    content += "data-table='"+this.table+"' ";
-                
-                if (this.row)
-                    content += "data-row="+this.row;
-                
-                content +=">reload</a></li>";
-
-                $('#apiPath').html(content);
-
-                var self = this;
-
-                $('#apiPath a').click(function(){
-                    self.$parent.navigate(
-                        this.getAttribute('data-path'),
-                        'GET',
-                        '',
-                        this.getAttribute('data-table'),
-                        this.getAttribute('data-row')
-                    )
-                });
-
-            }
-        }
+        name: 'Nav',
+        
+        props: [ 'table', 'row', 'edit-mode', 'click' ]
 
     }
 </script>
